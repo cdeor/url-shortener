3386410e5ae699c12e83db8db73fbe3bb3a6ea84
Shubh <pandey.shubh10@gmail.com>
2025-03-03 21:14:24 +0530

    Added geturl route

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/cdeor/url-shortener/api/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"google.golang.org/genproto/googleapis/maps/routes/v1"
 )
 
 func main() {
@@ -12,4 +17,20 @@ func main() {
 		fmt.Println(err)
 	}
 
+	router := gin.Default()
+
+	setupRouters(router)
+
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(router.Run(":" + port))
+
+}
+
+func setupRouters(router *gin.Engine) {
+	router.POST("/api/v1", routes.ShortenURL)
+	router.GET("/api/v1/:shortID", routes.GetByShortID)
 }
